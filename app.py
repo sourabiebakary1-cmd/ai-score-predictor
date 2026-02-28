44112b53a5124c63bdd38638375f18f2c0eb6483
sourabiebakary1-cmd <sourabiebakary1@gmail.com>
2026-02-28 08:45:34 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-import streamlit as st st.title("Predicteur de Score IA") st.write("Bienvenue dans ton application") equipe1 = st.text_input("Nom equipe 1") equipe2 = st.text_input("Nom equipe 2") if st.button("Predire le score"):st.success("Score probable : 2 - 1")   
+import streamlit as st
+import random
+
+st.title("Prédicteur de score IA ⚽")
+st.write("Prédiction des scores EA Sports FC 25")
+
+team1 = st.text_input("Entrez le nom de l'équipe 1")
+team2 = st.text_input("Entrez le nom de l'équipe 2")
+
+if st.button("Prédire le score"):
+    if team1 and team2:
+        st.subheader("Top 3 Scores Probables 🔥")
+        for i in range(3):
+            score1 = random.randint(0, 4)
+            score2 = random.randint(0, 4)
+            st.write(f"{team1} {score1} - {score2} {team2}")
+    else:
+        st.warning("Veuillez entrer les deux équipes.")
