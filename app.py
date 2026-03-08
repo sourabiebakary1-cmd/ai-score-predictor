c8d4c15775ef346c846cc74352eb72053ec195e7
sourabiebakary1-cmd <sourabiebakary1@gmail.com>
2026-03-08 19:05:45 +0000

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="Bakary Predictor Pro", layout="centered")
 st.title("⚽ BAKARY AI FOOTBALL PREDICTOR PRO")
 st.header("📅 Matchs du jour")
 
-API_KEY = "TA_CLE_RAPIDAPI"
+API_key: cc99563a7dmsh7b90e353380edb4p113eb4jsnf530a296e8c2'
 
 url = "https://free-api-live-football-data.p.rapidapi.com/football-fixtures"
 
