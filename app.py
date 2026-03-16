25331373dc4c0154804ccb41b0c4326bc59e71e1
sourabiebakary1-cmd <sourabiebakary1@gmail.com>
2026-03-16 13:13:32 +0000

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,7 @@ st.set_page_config(page_title="BAKARY AI FOOTBALL PRO V7", layout="wide")
 st.title("BAKARY AI FOOTBALL PRO V7")
 st.success("IA Football professionnelle")
 
-TA CLE API
-
+# TA CLE API
 API_KEY = "289e8418878e48c598507cf2b72338f5"
 
 headers = {
